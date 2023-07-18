ee42f12dfedb82bb26fe5ab330bfb894053d21e5
tijnbarthen <tijn@directsale.nl>
2023-07-18 14:28:17 +0200

    all stylinh done now

--- components/hero.tsx
+++ components/hero.tsx
@@ -14,62 +14,60 @@ export default function Hero() {
         className="absolute inset-0 -z-10 h-full w-full object-cover"
       />
 
-      <div className="max-w-6xl mx-auto px-4 sm:px-6">
-        <div className="pt-32 pb-12 md:pt-40 md:pb-20 flex justify-center">
-          {/* Hero content */}
-          <div className="relative max-w-xl mx-auto md:max-w-none text-center flex flex-col md:flex-row">
-            {/* Content */}
-            <div className="md:w-[800px]">
-              <h1
-                className="h1 font-cabinet-grotesk mb-6 text-3xl sm:text-3xl md:text-4xl text-white lg:text-5xl xl:text-6xl"
-                data-aos="fade-right"
-                data-aos-delay="200"
-              >
-                Verhuur | Beheer | Aanhuur
-                {/* <span
+      <div className="max-w-6xl mx-auto px-4 sm:px-6 h-full flex justify-center items-center">
+        {/* Hero content */}
+        <div className="max-w-xl mx-auto md:max-w-none text-center flex flex-col justify-center items-center md:flex-row min-h-[60vh] h-full">
+          {/* Content */}
+          <div className="md:w-[800px]">
+            <h1
+              className="h1 font-cabinet-grotesk mb-6 text-3xl sm:text-3xl md:text-4xl text-white lg:text-5xl xl:text-6xl"
+              data-aos="fade-right"
+              data-aos-delay="200"
+            >
+              Verhuur | Beheer | Aanhuur
+              {/* <span
                   className="relative inline-flex text-white h1 font-cabinet-grotesk mb-6 text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl  bg-blue-950 px-3"
                   data-aos="fade-right"
                 ></span> */}
-              </h1>
-              {/* <p
+            </h1>
+            {/* <p
                 className="text-2xl text-white mb-10 font-cabinet-grotesk font-bold"
                 data-aos="fade-right"
                 data-aos-delay="200"
               >
                 Linden Vastgoed jouw partner in vastgoed.
               </p> */}
-              <div
-                className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center md:justify-center space-y-4 sm:space-y-0 sm:space-x-4 mb-12 md:mb-20"
-                data-aos="fade-right"
-                data-aos-delay="300"
-              >
-                <div>
-                  <Link
-                    href="/aanbod"
-                    className="btn text-white bg-blue-300 hover:bg-blue-600 w-full shadow-sm"
-                    data-aos="fade-left"
-                  >
-                    Verhuur
-                  </Link>
-                </div>
-                <div>
-                  <Link
-                    className="btn text-white bg-blue-300 hover:bg-blue-600 w-full shadow-sm "
-                    href="/onze-diensten"
-                    data-aos="fade-right"
-                  >
-                    Aanhuur
-                  </Link>
-                </div>
-                <div>
-                  <Link
-                    className="btn text-white bg-blue-300 hover:bg-blue-600 w-full shadow-sm "
-                    href="/onze-diensten"
-                    data-aos="fade-right"
-                  >
-                    Beheer
-                  </Link>
-                </div>
+            <div
+              className="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center md:justify-center space-y-4 sm:space-y-0 sm:space-x-4 "
+              data-aos="fade-right"
+              data-aos-delay="300"
+            >
+              <div>
+                <Link
+                  href="/aanbod"
+                  className="btn text-white bg-blue-300 hover:bg-blue-600 w-full shadow-sm"
+                  data-aos="fade-left"
+                >
+                  Verhuur
+                </Link>
+              </div>
+              <div>
+                <Link
+                  className="btn text-white bg-blue-300 hover:bg-blue-600 w-full shadow-sm "
+                  href="/onze-diensten"
+                  data-aos="fade-right"
+                >
+                  Aanhuur
+                </Link>
+              </div>
+              <div>
+                <Link
+                  className="btn text-white bg-blue-300 hover:bg-blue-600 w-full shadow-sm "
+                  href="/onze-diensten"
+                  data-aos="fade-right"
+                >
+                  Beheer
+                </Link>
               </div>
             </div>
           </div>
